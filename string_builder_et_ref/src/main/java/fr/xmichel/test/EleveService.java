6322f9b8c127bb7b1dd502a1a59a595115ab4699
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2013-01-18 13:24:16 +0100

    demo 3

---
+++ string_builder_et_ref/src/main/java/fr/xmichel/test/EleveService.java
@@ -0,0 +1,13 @@
+package fr.xmichel.test;
+
+public interface EleveService {
+
+	/**
+	 * Passe un examen
+	 * 
+	 * @return
+	 * 			La note obtenue
+	 */
+	public int passerExamen(Eleve e);
+
+}
---
+++ string_builder_et_ref/src/main/java/fr/xmichel/test/EleveServiceImpl.java
@@ -0,0 +1,27 @@
+package fr.xmichel.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EleveServiceImpl implements EleveService {
+
+	private static final Logger logger = LoggerFactory.getLogger(EleveServiceImpl.class);
+	
+	@Override
+	public int passerExamen(Eleve e) {
+		logger.debug("L'eleve passe un examen...");
+		
+		// du code...
+		logger.trace("Cet examen n'est pas facile !");
+		// du code...
+		
+		int noteFinale = 10;
+		
+		e.addNote(noteFinale);
+		
+		logger.debug("On a obtenu la note : {}", noteFinale);
+		logger.trace("e vaut maintenant {}", e);
+		
+		return noteFinale;
+	}
+}
